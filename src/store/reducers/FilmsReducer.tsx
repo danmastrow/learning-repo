d9b8eabbd01d5d1158e61e6d99c65c4e8a89b956
Daniel <15012375+danmastrow@users.noreply.github.com>
2019-12-28 10:47:35 +1100

    Add comment about generic slice creation

--- src/store/reducers/FilmsReducer.tsx
+++ src/store/reducers/FilmsReducer.tsx
@@ -16,6 +16,7 @@ const initialState: FilmsState = {
   loading: false,
   errored: false
 };
+// TODO: turn this into a generic slice creation, getX, getXSuccess, getXFailure
 const films = createSlice({
   name: "films",
   initialState: initialState,
